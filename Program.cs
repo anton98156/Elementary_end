87fe37365128d725d4b71935cf59e7c6e315896d
Anton <anton98156789@gmail.com>
2022-11-24 14:59:03 +0300

    create array@ clear

--- Program.cs
+++ Program.cs
@@ -9,3 +9,24 @@ PrintArray(array);
 Console.WriteLine();
 PrintArray(Change (array));
 
+string [] ReadArray (){
+    Console.Write("Enter length of array: ");
+    string lenStr = Console.ReadLine();
+    int len = Convert.ToInt32(lenStr);
+
+    string [] result = new string[len];
+    for (int i = 0; i < len; i++)
+    {
+        string input = Console.ReadLine();
+        result[i] = input;
+    }
+    return result;
+}
+
+void PrintArray(string [] array){
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine ($"{array[i]}");
+
+    }
+}
